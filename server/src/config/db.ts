91c7a3a6d1f12c88a9fea6e4d5978b05f6ac2f30
breitano <braydon@motiveit.com.au>
2025-12-10 15:25:11 +1100

    Setup infrastructure. Confirmed can connect to MongoDB correctly

---
+++ server/src/config/db.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+export const connectDB = async (): Promise<void> => {
+  try {
+    const uri = process.env.MONGO_URI;
+
+    if (!uri) {
+      throw new Error("MONGO_URI not defined");
+    }
+
+    await mongoose.connect(uri);
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  }
+};
